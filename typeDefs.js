378631e1af53e80e581297a0929488287a514e37
Glen Rutherford <glenrutherford4@gmail.com>
2021-01-29 10:34:35 +1100

    Init

---
+++ typeDefs.js
@@ -0,0 +1,43 @@
+export const typeDefs = /* GraphQL */ `
+  type Business {
+    businessId: ID!
+    name: String
+    address: String
+    avgStars: Float
+    photos(first: Int = 3, offset: Int = 0): [Photo]
+    reviews(first: Int = 3, offset: Int = 0): [Review]
+  }
+  type User {
+    userId: ID!
+    name: String
+    photos: [Photo]
+    reviews: [Review]
+  }
+  type Photo {
+    business: Business
+    user: User
+    photoId: ID!
+    url: String
+  }
+  type Review {
+    reviewId: ID!
+    stars: Float
+    text: String
+    user: User
+    business: Business
+  }
+  enum BusinessOrdering {
+    name_asc
+    name_desc
+  }
+  type Query {
+    allBusinesses(first: Int = 10, offset: Int = 0): [Business]
+    businessBySearchTerm(
+      search: String
+      first: Int = 10
+      offset: Int = 0
+      orderBy: BusinessOrdering = name_asc
+    ): [Business]
+    userById(id: ID!): User
+  }
+`;
