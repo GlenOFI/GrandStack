4b33fb9001e0a26dcfb9fdbf53b522db7d7b575d
Glen Rutherford <grutherford@ofigroup.com.au>
2021-02-01 15:48:04 +1100

    UI done

--- UI/web-react/src/App.js
+++ UI/web-react/src/App.js
@@ -1,23 +1,58 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import BusinessResults from "./BusinessResults";
+
+const businesses = [
+  {
+    businessId: "b1",
+    name: "San Mateo Public Library",
+    address: "55 W 3rd Ave",
+    category: "Library",
+  },
+  {
+    businessId: "b2",
+    name: "Ducky's Car Wash",
+    address: "716 N San Mateo Dr",
+    category: "Car Wash",
+  },
+  {
+    businessId: "b3",
+    name: "Hanabi",
+    address: "723 California Dr",
+    category: "Restaurant",
+  },
+];
 
 function App() {
+  const [selectedCategory, setSelectedCategory] = useState("All");
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Business Search</h1>
+      <form>
+        <label>
+          Select Business Category:
+          <select
+            value={selectedCategory}
+            onChange={(event) => setSelectedCategory(event.target.value)}
+          >
+            <option value="All">All</option>
+            <option value="Library">Library</option>
+            <option value="Restaurant">Restaurant</option>
+            <option value="Car Wash">Car Wash</option>
+          </select>
+        </label>
+
+        <input type="submit" value="Submit" />
+      </form>
+
+      <BusinessResults
+        businesses={
+          selectedCategory === "All"
+            ? businesses
+            : businesses.filter((b) => {
+                return b.category === selectedCategory;
+              })
+        }
+      />
     </div>
   );
 }
